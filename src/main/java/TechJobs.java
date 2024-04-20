ca360e0d4240dd5ceb1bd57ebcfe1d8e1edfad95
Tim-E <caffeinated.ink@gmail.com>
2024-04-20 16:11:18 -0400

    First commit, testing workflow/actions

--- src/main/java/TechJobs.java
+++ src/main/java/TechJobs.java
@@ -77,7 +77,7 @@ public class TechJobs {
         Boolean validChoice = false;
         String[] choiceKeys = new String[choices.size()];
 
-        // Put the choices in an ordered structure so we can
+        // Put the choices in an ordered structure, so we can
         // associate an integer with each one
         int i = 0;
         for (String choiceKey : choices.keySet()) {
